d0b600f57ea9d794704e1cacb1ddde0f5b24001d
JP Miller <legendzing@gmail.com>
2015-06-17 10:34:59 -0500

    Update app.js
    created random integer function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// establish string variables
 var fizz = "fizz";             /* 3 */
 var buzz = "buzz";             /* 5 */
 var fizzBuzz = "Fizzbuzz";     /* 3 & 5 */
@@ -5,8 +6,14 @@ var sixer = "sixer";           /* 6 */
 var nixer = "nixer";           /* 9 */
 var sixerNixer = "sixerNixer"; /* 6 & 9 */
 
+// function creates a random integer
+function getRandomInt(min, max) {
+			return Math.floor(Math.random() * (max - min)) + min;
+		}
 
-for (var i=1; i <= 144; i++) {
+// calls getRandomInt between 1 & 144 & pushes through fizzBuzzer
+		var i = getRandomInt(1, 145);
+// for (var i=1; i <= 144; i++) { commenting out for now, may delete
   if (i % 3 === 0 && i % 5 === 0) {
     console.log(fizzBuzz);
   } else if (i % 3 === 0) {
@@ -15,5 +22,5 @@ for (var i=1; i <= 144; i++) {
     console.log(buzz);
   } else {
     console.log(i);
-  }
+  // } for line curly brace
 }
