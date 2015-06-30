5b8471d22178beb323cbd67879ff9b68df233776
JP Miller <legendzing@gmail.com>
2015-06-29 23:28:18 -0500

    feat: prints results to website in table

--- js/app.js
+++ js/app.js
@@ -15,16 +15,23 @@ function getRandomInt(min, max) {
 function integerChecker() {
 	if (i % 3 === 0 && i % 5 === 0) {
 		console.log(fizzBuzz);
+		document.getElementById("number").innerHTML = fizzBuzz;
 	} else if (i % 3 === 0) {
 		console.log(fizz);
+		document.getElementById("number").innerHTML = fizz;
 	} else if (i % 5 === 0) {
 		console.log(buzz);
+		document.getElementById("number").innerHTML = buzz;
 	} else {
 		console.log(integer);
+		document.getElementById("number").innerHTML = integer;
 	}
 };
 
-// calls getRandomInt() for a number between 1 & 144 and integerChecker()
+
+
+// calls getRandomInt and pushes through fizzBuzzer
 var i = getRandomInt(1, 145);
 console.log(i);
+document.getElementById("computer-number").innerHTML = i;
 integerChecker();
